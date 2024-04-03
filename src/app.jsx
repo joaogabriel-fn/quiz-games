20ac7bf63f415376325894242630d3a9ff29c03e
Joao Gabriel <joaogabriel.farias@Hotmail.com>
2024-04-03 10:46:16 -0300

    Static design added

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,18 @@
-const App = () => <h1>Boilerplate Vite + React</h1>;
+const App = () => (
+  <div className="app">
+    <main className="main">
+      <h4>Pergunta</h4>
+      <div className="options">
+        <button className="btn btn-option">Resposta</button>
+        <button className="btn btn-option">Resposta</button>
+        <button className="btn btn-option">Resposta</button>
+        <button className="btn btn-option">Resposta</button>
+      </div>
+      <div>
+        <button className="btn btn-ui">Próxima</button>
+      </div>
+    </main>
+  </div>
+);
 
 export { App };
